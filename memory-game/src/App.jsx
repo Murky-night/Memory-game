aaf74fb28e2a0ec2e2073ce12a8a50337ab407dc
Tran Dang Khoi <khoi23122005@gmail.com>
2026-03-02 10:26:30 +0700

    update card flip

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,5 +1,6 @@
 import { GameHeader } from "./components/GameHeader"
 import { Card } from "./components/Card";
+import { useEffect, useState } from "react";
 
 function App() {
 
@@ -22,14 +23,54 @@ function App() {
   "🍒",
   ];
 
+  const [cards, setCards] = useState([]);
+
+  const initializeGame = () => {
+    // SHUFFLE THE CARDS
+    // const shuffled = shuffleArray(cardValues);
+
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+
+    setCards(finalCards);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card) => {
+    // Don't allow clicking if card is already flipped, matched
+    if (
+      card.isFlipped ||
+      card.isMatched
+    ) {
+      return;
+    }
+
+    // Update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards)
+  };
 
   return (
     <div className="app">
       <GameHeader score={3} move={10} />
 
       <div className="cards-grid">
-        {cardValues.map((card) => (
-          <Card card={card} />
+        {cards.map((card) => (
+          <Card card={card} onClick={handleCardClick} />
         ))}
       </div>
     </div>
