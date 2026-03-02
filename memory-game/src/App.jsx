893011461572eedd8d305eb2fa240e3c51327d53
Tran Dang Khoi <khoi23122005@gmail.com>
2026-03-02 13:48:34 +0700

    import logic to a separate file

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -2,9 +2,7 @@ import { GameHeader } from "./components/GameHeader"
 import { Card } from "./components/Card";
 import { useEffect, useState } from "react";
 
-function App() {
-
-  const cardValues = [
+const cardValues = [
   "🍎",
   "🍌",
   "🍇",
@@ -23,95 +21,7 @@ function App() {
   "🍒",
   ];
 
-  const [cards, setCards] = useState([]);
-  const [flippedCards, setFlippedCards] = useState([]);
-
-  const initializeGame = () => {
-    // SHUFFLE THE CARDS
-    // const shuffled = shuffleArray(cardValues);
-
-    const finalCards = cardValues.map((value, index) => ({
-      id: index,
-      value,
-      isFlipped: false,
-      isMatched: false,
-    }));
-
-    setCards(finalCards);
-  };
-
-  useEffect(() => {
-    initializeGame();
-  }, []);
-
-  const handleCardClick = (card) => {
-    // Don't allow clicking if card is already flipped, matched
-    if (
-      card.isFlipped ||
-      card.isMatched ||
-      flippedCards.length === 2
-    ) {
-      return;
-    }
-
-    // Update card flipped state
-    const newCards = cards.map((c) => {
-      if (c.id === card.id) {
-        return { ...c, isFlipped: true };
-      } else {
-        return c;
-      }
-    });
-
-    setCards(newCards)
-
-    const newFlippedCards = [...flippedCards, card.id];
-    setFlippedCards(newFlippedCards);
-
-    // Check for match if two cards are flipped
-
-    if (flippedCards.length === 1) {
-      setIsLocked(true);
-      const firstCard = cards[flippedCards[0]];
-
-      if (firstCard.value === card.value) {
-        setTimeout(() => {
-          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
-          setScore((prev) => prev + 1);
-          setCards((prev) =>
-            prev.map((c) => {
-              if (c.id === card.id || c.id === firstCard.id) {
-                return { ...c, isMatched: true };
-              } else {
-                return c;
-              }
-            })
-          );
-
-          setFlippedCards([]);
-          setIsLocked(false);
-        }, 500);
-      } else {
-        // flip back card 1, card 2
-
-        setTimeout(() => {
-          const flippedBackCard = newCards.map((c) => {
-            if (newFlippedCards.includes(c.id) || c.id === card.id) {
-              return { ...c, isFlipped: false };
-            } else {
-              return c;
-            }
-          });
-
-          setCards(flippedBackCard);
-          setIsLocked(false);
-          setFlippedCards([]);
-        }, 1000);
-      }
-  };
-}
-  
-    
+function App() {
 
   return (
     <div className="app">
