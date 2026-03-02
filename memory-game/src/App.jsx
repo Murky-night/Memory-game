56408c392e57363277a003e6065e9f03b6b9144b
Tran Dang Khoi <khoi23122005@gmail.com>
2026-03-02 09:03:02 +0700

    create components folder

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,22 +1,12 @@
+import { GameHeader } from "./components/GameHeader"
+
 function App() {
 
   return (
-    <>
-      <div className="game-header">
-        <h1>🎮 Memory card game</h1>
-        <div className="stats">
-          <div className="stat-item">
-            <div className="stat-label">Score: </div>
-            <div className="stat-value">0</div>
-          </div>
-          <div className="stat-item">
-            <div className="stat-label">Moves: </div>
-            <div className="stat-value">0</div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    <div className="app">
+      <GameHeader />
+    </div>
+  );
 }
 
 export default App
---
+++ memory-game/src/components/GameHeader.jsx
@@ -0,0 +1,17 @@
+export const GameHeader = () => {
+    return (
+        <div className="game-header">
+        <h1>🎮 Memory card game</h1>
+        <div className="stats">
+          <div className="stat-item">
+            <div className="stat-label">Score: </div>
+            <div className="stat-value">0</div>
+          </div>
+          <div className="stat-item">
+            <div className="stat-label">Moves: </div>
+            <div className="stat-value">0</div>   
+          </div>
+        </div>
+      </div>
+    );
+};
